c906843e6b09d17cc9d10098bcad38392033bfee
Old <nko.goh@gmail.com>
2017-08-26 00:52:49 +0200

    fix firefox conditional isReady

--- src/app/class/PointDeVue.ts
+++ src/app/class/PointDeVue.ts
@@ -59,8 +59,8 @@ export class PointDeVue{
     this.getVideoBuffer();
     let range = findClosestRange(this, g.currentFrame);
     let readyStateMin = g.isFirefox ? 3 : 4;
-    let audioReady = (this.audio.readyState == readyStateMin || this.audio.currentTime === this.audio.duration);
-    let videoReady = (this.audio.readyState == readyStateMin || this.video.currentTime === this.video.duration);
+    let audioReady = (this.audio.readyState >= readyStateMin || this.audio.currentTime === this.audio.duration);
+    let videoReady = (this.audio.readyState >= readyStateMin || this.video.currentTime === this.video.duration);
     let bool = this.fin < g.currentFrame ||( audioReady  && videoReady);
       // &&(!isPointDeVueAvailable(this, g.currentFrame)
       //   || (range  && range[1] > g.currentFrame - this.depart + CST.MIN_BUFFER_FRAMES)
